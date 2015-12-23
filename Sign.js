b629912a029bd46f052efcf9837462856f296e29
Louis <qingyanjiu@126.com>
2015-12-23 17:08:34 +0800

    修改了一点代码

--- Sign.js
+++ Sign.js
@@ -10,30 +10,33 @@ var {
   AppRegistry,
   StyleSheet,
   View,
+  Image
 } = React;
 
 
 var Sign = React.createClass({
-  mixins: [tweenState.Mixin],
+  mixins: [tweenState.Mixin,TimerMixin],
 
   getInitialState:function(){   
       return({
         r:100,
         opacity:1,
+        sr:100,
+        sopacity:1,
       });
   },
 
   animationShow:function(){
     this.tweenState('opacity', {
         easing: tweenState.easingTypes.easeOutQuint,
-        duration: 2000,
-        endValue: 0,
+        duration: 3000,
+        endValue: 0.01,
         onEnd:this.setState({r:100,opacity:1})
       });
       this.tweenState('r', {
         easing: tweenState.easingTypes.easeOutQuint,
-        duration: 2000,
-        endValue: Dimensions.get('window').width+20,
+        duration: 3000,
+        endValue: Dimensions.get('window').width,
         onEnd:this.animationShow,
       });
   },
@@ -45,10 +48,12 @@ var Sign = React.createClass({
   render: function() {
    return (
         <View style={styles.container}>
-          <View style={[styles.circle,{width:this.getTweeningValue('r'),height:this.getTweeningValue('r'),borderRadius:this.getTweeningValue('r')/2,opacity: this.getTweeningValue('opacity'),}]}>
+          <View style={[styles.circle,{width:this.getTweeningValue('r'),height:this.getTweeningValue('r'),borderRadius:this.getTweeningValue('r')/2,opacity: this.getTweeningValue('opacity'),alignItems:'center',justifyContent:'center'}]}>
+           
           </View>
-          <View style={{backgroundColor:'white',height:100,width:100,borderRadius:50,position: 'absolute',top:Dimensions.get('window').height/2-50,left:Dimensions.get('window').width/2-50}}>
-          </View>
+
+          <Image style={{backgroundColor:'white',height:100,width:100,borderRadius:50,position: 'absolute',top:Dimensions.get('window').height/2-50,left:Dimensions.get('window').width/2-50}} source={require('./img/sign.png')}/> 
+
         </View>      
       );
     }
@@ -57,14 +62,13 @@ var Sign = React.createClass({
 
 var styles = StyleSheet.create({
   container:{
-    backgroundColor:'48BBEC',
-    padding:30,
+    backgroundColor:'white',
     flex:1,
     justifyContent:'center',
     alignItems:'center'
   },
   circle: {
-    backgroundColor:'red',
+    backgroundColor:'blue',
   }
 });
 

ae04f1e98ea51a3208841fc8efc16f6278b2dd83
Louis <qingyanjiu@126.com>
2016-12-13 14:32:11 +0800

    修改了配置文件

--- android/app/src/main/java/com/reading/MainActivity.java
+++ android/app/src/main/java/com/reading/MainActivity.java
@@ -1,15 +1,21 @@
 package com.reading;
 
-import com.facebook.react.ReactActivity;
-import cl.json.RNSharePackage;
+import android.os.Bundle;
+
 import com.eguma.barcodescanner.BarcodeScanner;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import io.realm.react.RealmReactPackage;
+import com.facebook.react.LifecycleState;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.ReactRootView;
 import com.facebook.react.shell.MainReactPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 
 import java.util.Arrays;
 import java.util.List;
+
+import cl.json.RNSharePackage;
+import io.realm.react.RealmReactPackage;
 
 public class MainActivity extends ReactActivity {
 
@@ -38,11 +44,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new RNSharePackage(),
-            new BarcodeScanner(),
-            new RCTCameraPackage(),
-            new RealmReactPackage()
+                new MainReactPackage(),
+                new RNSharePackage(),
+                new BarcodeScanner(),
+                new RCTCameraPackage(),
+                new RealmReactPackage()
         );
     }
 }

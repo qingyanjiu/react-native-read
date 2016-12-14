67abab824df6478e1b60c88faefe35034cb09e58
Louis <qingyanjiu@126.com>
2016-12-14 09:50:40 +0800

    增加了npm包和修改了后台服务器信息

--- readPage/Constants.js
+++ readPage/Constants.js
@@ -1,6 +1,6 @@
 var Constants = {
   //服务器url
-  URL : 'http://alaien-book.daoapp.io',
+  URL : 'http://alaien-reading-server.daoapp.io',
 
   // URL : 'https://ntizyc-3000-eojfft.box.myide.io',
 
--- readPageAndroid/Constants.js
+++ readPageAndroid/Constants.js
@@ -2,7 +2,7 @@ var Constants = {
   //服务器url
   //URL : 'http://alaien-book.daoapp.io',
 
-   URL : 'https://ntizyc-3000-minldp.box.myide.io',
+   URL : 'http://alaien-reading-server.daoapp.io',
 
 
   
